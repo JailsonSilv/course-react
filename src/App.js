ba8f033f25c972d07497d301c42ffb8df7d7e778
Jailson Silva <104876290+JailsonSilv@users.noreply.github.com>
2022-05-25 10:13:36 -0300

    Fundamentos

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Components
+import FirstComponent from "./components/FirstComponent";
+
+// Styles / Css
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
